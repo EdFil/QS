3570eadc0689d1e9d0f7acd681e2fe347458cea2
Edgar Filipe Ramires dos Santos <edfil221@gmail.com>
2014-10-31 14:47:45 +0000

    Finished Rstaurant

--- Proj1/RestaurantTest.java
+++ Proj1/RestaurantTest.java
@@ -19,10 +19,10 @@ public class RestaurantTest {
 	public static final String DEFAULT_RESTAURANT_ADRESS = "Food Street, after the tree";
 
 	@Test
-	public void testCase1TestingConstructorWithValidData(){
+	public void testCase1TestingWithValidData(){
 		List<Dish> dishList = Arrays.asList(NORM_1, NORM_2, NORM_3, NORM_4, NORM_5);
 		int[] dishPrices = new int[] { 22, 20, 30 ,40, 50 };
-		Restaurant vegiRestaurant = new Restaurante(DEFAULT_RESTAURANT_NAME, DEFAULT_RESTAURANT_ADRESS, dishList, dishPrices);
+		Restaurant restaurant = new Restaurante(DEFAULT_RESTAURANT_NAME, DEFAULT_RESTAURANT_ADRESS, dishList, dishPrices);
 		assertEquals("Restaurant name is not OK", DEFAULT_RESTAURANT_NAME, restaurant.getName());
 		assertEquals("Restaurant address is not OK", DEFAULT_RESTAURANT_ADRESS, restaurant.getAdress());
 		assertEquals("Restaurant should not be Vegetarian by default", false, restaurant.isVegetarian());
@@ -31,6 +31,71 @@ public class RestaurantTest {
 			assertEquals("Price " + i + " is not OK", dishPrices.get(i), getPrice(dishList.get(i)));
 		}
 	}
+
+	@Test
+	public void testCase2TestingNotEnoughtDishes(){
+		List<Dish> dishList = Arrays.asList(NORM_1, NORM_2, NORM_3, NORM_4);
+		int[] dishPrices = new int[] { 21, 19, 30 , 29 };
+		try{
+			Restaurant restaurant = new Restaurante(DEFAULT_RESTAURANT_NAME, DEFAULT_RESTAURANT_ADRESS, dishList, dishPrices);
+			fail("Restaurant should not have been created");
+		}catch(InvalidInvocationException e) {}
+	}
+
+	@Test
+	public void testCase5TestingOneFreePrice(){
+		List<Dish> dishList = Arrays.asList(NORM_1, NORM_2, NORM_3, NORM_4, NORM_5, VEGI_1, VEGI_2);
+		int[] dishPrices = new int[] { 10, 12, 132, 53, 32, 1, 34 };
+		Restaurant restaurant = new Restaurante(DEFAULT_RESTAURANT_NAME, DEFAULT_RESTAURANT_ADRESS, dishList, dishPrices);
+		restaurant.addDish(NORM_2, 0);
+		for(int i = 0; i < dishList.size; i++){
+			assertEquals("Price " + i + " is not OK", dishPrices.get(i), getPrice(dishList.get(i)));
+			assertEquals("Dish " + i + " is not OK", dishList.get(i), restaurant.getDishes().getDish(i));
+		}
+		assertEquals("Restaurant should not be Vegetarian by default", false, restaurant.isVegetarian());
+		assertEquals("Restaurant address is not OK", DEFAULT_RESTAURANT_ADRESS, restaurant.getAdress());
+		assertEquals("Restaurant name is not OK", DEFAULT_RESTAURANT_NAME, restaurant.getName());
+	}
+
+	@Test
+	public void testCase10TestingNotEnoughPricesForDishes(){
+		List<Dish> dishList = Arrays.asList(NORM_1, NORM_2, NORM_3, NORM_4, NORM_5, VEGI_1);
+		int[] dishPrices = new int[] { 10, 12, 132, 53, 32 };
+		try{
+			Restaurant restaurant = new Restaurante(DEFAULT_RESTAURANT_NAME, DEFAULT_RESTAURANT_ADRESS, dishList, dishPrices);
+		}catch(InvalidInvocationException e) {}
+	}
+
+	@Test
+	public void testCase13TestingSettingVegitarianWithNonVegitarianFood(){
+		List<Dish> dishList = Arrays.asList(VEGI_1, VEGI_2, VEGI_3, VEGI_4, VEGI_5, NORM_1, NORM_2);
+		int[] dishPrices = new int[] { 21, 10, 0, 14, 21, 92, 18 };
+		Restaurant restaurant = new Restaurante(DEFAULT_RESTAURANT_NAME, DEFAULT_RESTAURANT_ADRESS, dishList, dishPrices);
+		try {
+			restaurant.setVegetarian(true);
+			fail("Cannot have a vegetaria restaurant with non vegetarian food");
+		} catch(InvalidInvocationException e) {
+			assertFalse("Restaurant should not have changed to vegitarian", restaurant.isVegetarian());
+		}
+	}
+
+	@Test
+	public void testCase15TestingHavingNonUniqueDishNames(){
+		Dish dish = new Dish("Name1", "Desc1", true);
+		Dish dish2 = new Dish("Name1", "Desc2", false);
+		List<Dish> dishList = Arrays.asList(VEGI_1, VEGI_2, VEGI_3, VEGI_4);
+		int[] dishPrices = new int[] { 21, 10, 0, 14 };
+		Restaurant restaurant = new Restaurante(DEFAULT_RESTAURANT_NAME, DEFAULT_RESTAURANT_ADRESS, dishList, dishPrices);
+		restaurant.addDish(dish, 10);
+		try {
+			restaurant.addDish(dish2, 123);
+			fail("Cannot have 2 dishes with the same name");
+		} catch(InvalidInvocationException e) {
+			assertFalse("Restaurant should not have changed to vegitarian", restaurant.isVegetarian());
+			assertTrue("Dish should have not been removed", restaurant.getDishes().contains(dish));
+			assertFalse("Cannot have 2 dishes with the same name", restaurant.getDishes().contains(dish2));
+		}
+	}	
 
 }
 
